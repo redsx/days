bece7a0ad3f30af15427d4f391ed8535f06e2c43
condy <15979063786@163.com>
2017-06-12 20:40:06 +0800

    styles

---
+++ src/days/utils/styles.js
@@ -0,0 +1,56 @@
+const styles =  (function(){
+    function _border(direction = '') {
+        return (width = 0, style = 'hidden', color = 'black') => {
+            const styles = {};
+            const borderWidth = `border${direction}Width`,
+                borderColor = `border${direction}Color`,
+                borderStyle = `border${direction}Style`;
+            styles[borderWidth] = width;
+            styles[borderColor] = color;
+            styles[borderStyle] = style;
+            return styles;
+        }
+    }
+    function _setModel(start='', end='') {
+        const _attr = ['Top','Left','Bottom','Right'];
+        const attr = _attr.map(a => `${start}${start ? a : a.toLowerCase()}${end}`);
+        return function(width = 0) {
+            const style = {};
+            let label = 0;
+            for(let i=0; i<4; ++i){
+                let val = arguments[label];
+                if(!val){
+                    label = 0;
+                    val = arguments[label];
+                }
+                label++;
+                style[attr[i]] = val;
+            }
+            return style;
+        }
+    }
+    function layout(modal){
+        switch(modal){
+            case 'center':
+            default: {
+                return {
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }
+            }
+        }
+    }
+    return {
+        border: _border(),
+        borderLeft: _border('Left'),
+        borderRight: _border('Right'),
+        borderTop: _border('Top'),
+        borderBottom: _border('Bottom'),
+        borderWidth: _setModel('border','Width'),
+        padding: _setModel('padding'),
+        margin: _setModel('margin'),
+        layout: layout,
+    }
+})()
+
+export default styles;
